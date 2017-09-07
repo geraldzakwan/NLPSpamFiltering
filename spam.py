125ed542391115fcf02904b57e1bbc9268e9fb1c
geraldzakwan <geraldi_dzakwan@yahoo.co.id>
2017-09-07 11:56:23 +0700

    preprocessing and build word dictionary done

--- spam.py
+++ spam.py
@@ -1,25 +1,67 @@
-import json,csv
+import sys
+import json
+import csv
 import nltk
 from nltk.stem import WordNetLemmatizer
 
+# reload(sys)
+# sys.setdefaultencoding("utf-8")
 # nltk.download()
-if __name__ == '__main__':
-    # get json file stop words
+stopwords_dict = {}
+punctuation_dict = {}
+
+def is_ascii(s):
+    return all(ord(c) < 128 for c in s)
+
+def is_stopword(s):
+    return s in stopwords_dict
+
+def is_containing_punctuation(s):
+    for key in punctuation_dict:
+        if(key in s):
+            return True
+    return False
+
+def build_word_dictionary():
     file = open('stopwords-en.json','r+')
     stopwords_list = []
     stopwords_list = json.load(file)
-    # print(stopwords_list)
     file.close()
+
+    for stopword in stopwords_list:
+        stopwords_dict[stopword] = True
+
+    file = open('punctuation-en.json','r+')
+    punctuation_list = []
+    punctuation_list = json.load(file)
+    file.close()
+
+    for punctuation in punctuation_list:
+        punctuation_dict[punctuation] = True
+
+    # Build word dictionary
+    word_dictionary = {}
     lemmatizer = WordNetLemmatizer()
-    print(lemmatizer.lemmatize('monsters'))
-    # with open('spam.csv') as spamcsv:
-        #readCSV = csv.reader(csvfile,delimiter=',,,')
-        # for row in readCSV:
-            # lemmatization
-            # filterword = lemmatizer.lemmatize()
 
-            # hapus kata2 dari filterword jika ada di list stop words
+    with open('spam.csv', 'rb') as spamcsv:
+        readCSV = csv.reader(spamcsv)
+        for row in readCSV:
+            sentence = row[1]
+            if(is_ascii(sentence)):
+                tokens = nltk.word_tokenize(sentence)
 
-            # hapus tanda baca , ! & ? 
-            
-            # make dict
+                for token in tokens:
+                    if(is_ascii(token)):
+                        if(not is_stopword(token)):
+                            if(not is_containing_punctuation(token)):
+                                # Add to word dictionary
+                                token = lemmatizer.lemmatize(token)
+                                if(token in word_dictionary):
+                                    word_dictionary[token] = word_dictionary[token] + 1
+                                else:
+                                    word_dictionary[token] = 1
+
+    return word_dictionary
+
+if __name__ == '__main__':
+    print(build_word_dictionary())
