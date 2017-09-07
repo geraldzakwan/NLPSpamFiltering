fd205a72f117ceb8e53682d96c79c132aaefb372
ade_ramadhan <13514049@std.stei.itb.ac.id>
2017-09-07 12:25:59 +0700

    done preprocessing stopword,tanda baca,lemmatize

--- spam.py
+++ spam.py
@@ -2,24 +2,33 @@ import json,csv
 import nltk
 from nltk.stem import WordNetLemmatizer
 
+def make_dictionary(train_dir):
+    # train_dir as array of words all training data
+
+
 # nltk.download()
 if __name__ == '__main__':
     # get json file stop words
     file = open('stopwords-en.json','r+')
     stopwords_list = []
     stopwords_list = json.load(file)
+    punctuantion_list = ['!','&','?',]
     # print(stopwords_list)
     file.close()
+    filter_words = []
     lemmatizer = WordNetLemmatizer()
-    print(lemmatizer.lemmatize('monsters'))
-    # with open('spam.csv') as spamcsv:
-        #readCSV = csv.reader(csvfile,delimiter=',,,')
-        # for row in readCSV:
+    # print(lemmatizer.lemmatize('monsters'))
+    with open('spam.csv') as spamcsv:
+        readCSV = csv.reader(spamcsv)
+        for row in readCSV:
             # lemmatization
-            # filterword = lemmatizer.lemmatize()
-
-            # hapus kata2 dari filterword jika ada di list stop words
-
-            # hapus tanda baca , ! & ? 
-            
-            # make dict
+            # filterword = lemmatizer.lemmatize(row[1])
+            # print(row[0])
+            words = row[1].split()
+            # print(words)
+            # hapus kata2 dari filterword jika ada di list stop words            
+            for word in words:
+                if not word in stopwords_list and not word in punctuantion_list:
+                    word_lmt = lemmatizer.lemmatize(word)
+                    filter_words.append(word_lmt)
+            print(filter_words)
